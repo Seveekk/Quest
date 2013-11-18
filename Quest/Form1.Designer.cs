70c352aa72038af347336ce56de97daaaae6c6a6
Seweryn Kozlowski <seweryn.kozlowski@gmail.com>
2013-11-18 18:27:49 +0100

    Adding pcture box to the form

--- Quest/Form1.Designer.cs
+++ Quest/Form1.Designer.cs
@@ -47,9 +47,37 @@
             this.btnAttackRight = new System.Windows.Forms.Button();
             this.btnAttackLeft = new System.Windows.Forms.Button();
             this.btnAttackUp = new System.Windows.Forms.Button();
+            this.pictureBoxPlayer = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSword = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMace = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBow = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBat = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGhost = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGhoul = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPotionRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPotionBlue = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInvSword = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInvMace = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInvBow = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInvPotionRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInvPotionBlue = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGhost)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGhoul)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPotionRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPotionBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvSword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvMace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvBow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvPotionRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvPotionBlue)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -245,6 +273,188 @@
             this.btnAttackUp.Text = "Up";
             this.btnAttackUp.UseVisualStyleBackColor = true;
             // 
+            // pictureBoxPlayer
+            // 
+            this.pictureBoxPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxPlayer.Image = global::Quest.Properties.Resources.player;
+            this.pictureBoxPlayer.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxPlayer.Location = new System.Drawing.Point(83, 64);
+            this.pictureBoxPlayer.Name = "pictureBoxPlayer";
+            this.pictureBoxPlayer.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPlayer.TabIndex = 3;
+            this.pictureBoxPlayer.TabStop = false;
+            this.pictureBoxPlayer.Visible = false;
+            // 
+            // pictureBoxSword
+            // 
+            this.pictureBoxSword.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxSword.Image = global::Quest.Properties.Resources.sword;
+            this.pictureBoxSword.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxSword.Location = new System.Drawing.Point(119, 64);
+            this.pictureBoxSword.Name = "pictureBoxSword";
+            this.pictureBoxSword.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxSword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSword.TabIndex = 4;
+            this.pictureBoxSword.TabStop = false;
+            this.pictureBoxSword.Visible = false;
+            // 
+            // pictureBoxMace
+            // 
+            this.pictureBoxMace.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxMace.Image = global::Quest.Properties.Resources.mace;
+            this.pictureBoxMace.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxMace.Location = new System.Drawing.Point(155, 64);
+            this.pictureBoxMace.Name = "pictureBoxMace";
+            this.pictureBoxMace.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxMace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMace.TabIndex = 5;
+            this.pictureBoxMace.TabStop = false;
+            this.pictureBoxMace.Visible = false;
+            // 
+            // pictureBoxBow
+            // 
+            this.pictureBoxBow.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxBow.Image = global::Quest.Properties.Resources.bow;
+            this.pictureBoxBow.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxBow.Location = new System.Drawing.Point(191, 64);
+            this.pictureBoxBow.Name = "pictureBoxBow";
+            this.pictureBoxBow.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxBow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBow.TabIndex = 6;
+            this.pictureBoxBow.TabStop = false;
+            this.pictureBoxBow.Visible = false;
+            // 
+            // pictureBoxBat
+            // 
+            this.pictureBoxBat.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxBat.Image = global::Quest.Properties.Resources.bat;
+            this.pictureBoxBat.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxBat.Location = new System.Drawing.Point(227, 64);
+            this.pictureBoxBat.Name = "pictureBoxBat";
+            this.pictureBoxBat.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxBat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBat.TabIndex = 7;
+            this.pictureBoxBat.TabStop = false;
+            this.pictureBoxBat.Visible = false;
+            // 
+            // pictureBoxGhost
+            // 
+            this.pictureBoxGhost.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxGhost.Image = global::Quest.Properties.Resources.ghost;
+            this.pictureBoxGhost.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxGhost.Location = new System.Drawing.Point(263, 64);
+            this.pictureBoxGhost.Name = "pictureBoxGhost";
+            this.pictureBoxGhost.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxGhost.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGhost.TabIndex = 8;
+            this.pictureBoxGhost.TabStop = false;
+            this.pictureBoxGhost.Visible = false;
+            // 
+            // pictureBoxGhoul
+            // 
+            this.pictureBoxGhoul.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxGhoul.Image = global::Quest.Properties.Resources.ghoul;
+            this.pictureBoxGhoul.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxGhoul.Location = new System.Drawing.Point(299, 64);
+            this.pictureBoxGhoul.Name = "pictureBoxGhoul";
+            this.pictureBoxGhoul.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxGhoul.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGhoul.TabIndex = 9;
+            this.pictureBoxGhoul.TabStop = false;
+            this.pictureBoxGhoul.Visible = false;
+            // 
+            // pictureBoxPotionRed
+            // 
+            this.pictureBoxPotionRed.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxPotionRed.Image = global::Quest.Properties.Resources.potion_red;
+            this.pictureBoxPotionRed.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxPotionRed.Location = new System.Drawing.Point(335, 64);
+            this.pictureBoxPotionRed.Name = "pictureBoxPotionRed";
+            this.pictureBoxPotionRed.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxPotionRed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPotionRed.TabIndex = 10;
+            this.pictureBoxPotionRed.TabStop = false;
+            this.pictureBoxPotionRed.Visible = false;
+            // 
+            // pictureBoxPotionBlue
+            // 
+            this.pictureBoxPotionBlue.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxPotionBlue.Image = global::Quest.Properties.Resources.potion_blue;
+            this.pictureBoxPotionBlue.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxPotionBlue.Location = new System.Drawing.Point(371, 64);
+            this.pictureBoxPotionBlue.Name = "pictureBoxPotionBlue";
+            this.pictureBoxPotionBlue.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxPotionBlue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPotionBlue.TabIndex = 11;
+            this.pictureBoxPotionBlue.TabStop = false;
+            this.pictureBoxPotionBlue.Visible = false;
+            // 
+            // pictureBoxInvSword
+            // 
+            this.pictureBoxInvSword.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxInvSword.Image = global::Quest.Properties.Resources.sword;
+            this.pictureBoxInvSword.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxInvSword.Location = new System.Drawing.Point(74, 313);
+            this.pictureBoxInvSword.Name = "pictureBoxInvSword";
+            this.pictureBoxInvSword.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxInvSword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInvSword.TabIndex = 12;
+            this.pictureBoxInvSword.TabStop = false;
+            this.pictureBoxInvSword.Visible = false;
+            // 
+            // pictureBoxInvMace
+            // 
+            this.pictureBoxInvMace.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxInvMace.Image = global::Quest.Properties.Resources.mace;
+            this.pictureBoxInvMace.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxInvMace.Location = new System.Drawing.Point(130, 313);
+            this.pictureBoxInvMace.Name = "pictureBoxInvMace";
+            this.pictureBoxInvMace.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxInvMace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInvMace.TabIndex = 13;
+            this.pictureBoxInvMace.TabStop = false;
+            this.pictureBoxInvMace.Visible = false;
+            // 
+            // pictureBoxInvBow
+            // 
+            this.pictureBoxInvBow.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxInvBow.Image = global::Quest.Properties.Resources.bow;
+            this.pictureBoxInvBow.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxInvBow.Location = new System.Drawing.Point(186, 313);
+            this.pictureBoxInvBow.Name = "pictureBoxInvBow";
+            this.pictureBoxInvBow.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxInvBow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInvBow.TabIndex = 14;
+            this.pictureBoxInvBow.TabStop = false;
+            this.pictureBoxInvBow.Visible = false;
+            // 
+            // pictureBoxInvPotionRed
+            // 
+            this.pictureBoxInvPotionRed.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxInvPotionRed.Image = global::Quest.Properties.Resources.potion_red;
+            this.pictureBoxInvPotionRed.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxInvPotionRed.Location = new System.Drawing.Point(242, 313);
+            this.pictureBoxInvPotionRed.Name = "pictureBoxInvPotionRed";
+            this.pictureBoxInvPotionRed.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxInvPotionRed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInvPotionRed.TabIndex = 15;
+            this.pictureBoxInvPotionRed.TabStop = false;
+            this.pictureBoxInvPotionRed.Visible = false;
+            // 
+            // pictureBoxInvPotionBlue
+            // 
+            this.pictureBoxInvPotionBlue.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxInvPotionBlue.Image = global::Quest.Properties.Resources.potion_blue;
+            this.pictureBoxInvPotionBlue.InitialImage = global::Quest.Properties.Resources.player;
+            this.pictureBoxInvPotionBlue.Location = new System.Drawing.Point(298, 313);
+            this.pictureBoxInvPotionBlue.Name = "pictureBoxInvPotionBlue";
+            this.pictureBoxInvPotionBlue.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxInvPotionBlue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInvPotionBlue.TabIndex = 16;
+            this.pictureBoxInvPotionBlue.TabStop = false;
+            this.pictureBoxInvPotionBlue.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -252,15 +462,43 @@
             this.BackgroundImage = global::Quest.Properties.Resources.dungeon600x400;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(867, 399);
+            this.Controls.Add(this.pictureBoxInvPotionBlue);
+            this.Controls.Add(this.pictureBoxInvPotionRed);
+            this.Controls.Add(this.pictureBoxInvBow);
+            this.Controls.Add(this.pictureBoxInvMace);
+            this.Controls.Add(this.pictureBoxInvSword);
+            this.Controls.Add(this.pictureBoxPlayer);
+            this.Controls.Add(this.pictureBoxPotionBlue);
+            this.Controls.Add(this.pictureBoxPotionRed);
+            this.Controls.Add(this.pictureBoxGhoul);
+            this.Controls.Add(this.pictureBoxGhost);
+            this.Controls.Add(this.pictureBoxBat);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBoxSword);
+            this.Controls.Add(this.pictureBoxMace);
+            this.Controls.Add(this.pictureBoxBow);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGhost)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGhoul)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPotionRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPotionBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvSword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvMace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvBow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvPotionRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInvPotionBlue)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -286,6 +524,20 @@
         private System.Windows.Forms.Button btnAttackRight;
         private System.Windows.Forms.Button btnAttackUp;
         private System.Windows.Forms.Button btnAttackLeft;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer;
+        private System.Windows.Forms.PictureBox pictureBoxSword;
+        private System.Windows.Forms.PictureBox pictureBoxMace;
+        private System.Windows.Forms.PictureBox pictureBoxBow;
+        private System.Windows.Forms.PictureBox pictureBoxBat;
+        private System.Windows.Forms.PictureBox pictureBoxGhost;
+        private System.Windows.Forms.PictureBox pictureBoxGhoul;
+        private System.Windows.Forms.PictureBox pictureBoxPotionRed;
+        private System.Windows.Forms.PictureBox pictureBoxPotionBlue;
+        private System.Windows.Forms.PictureBox pictureBoxInvSword;
+        private System.Windows.Forms.PictureBox pictureBoxInvMace;
+        private System.Windows.Forms.PictureBox pictureBoxInvBow;
+        private System.Windows.Forms.PictureBox pictureBoxInvPotionRed;
+        private System.Windows.Forms.PictureBox pictureBoxInvPotionBlue;
     }
 }
 
